d7843fb4bfc303fa9e537617fefe18a830e1b268
Tez Smith <montez.smith@flatironschool.com>
2018-12-19 10:29:13 -0500

    Updated starter files

--- passenger.rb
+++ passenger.rb
@@ -2,28 +2,4 @@ class Passenger
 
   attr_reader :name
 
-@@all = []
-
-  def initialize(name)
-     @name = name
-     @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-#Get all the drivers associated with that passenger
-  def drivers
-    #First, I have to get all my rides
-     myrides = Ride.all.select do |ride|
-       ride.passenger == self
-     end
-
-     myrides.map do |ride|
-         ride.driver
-     end
-
-  end
-
 end
--- ride.rb
+++ ride.rb
@@ -2,17 +2,4 @@ class Ride
 
   attr_reader :driver, :passenger, :rating
 
-  @@all = []
-
-  def initialize(driver, passenger, rating)
-      @driver = driver
-      @passenger = passenger
-      @rating = rating
-      @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
 end
